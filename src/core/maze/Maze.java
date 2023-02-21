6933e0d2327bda2a092f954ed85a01e265f031a6
RichterDelaCruz <angelorichter.delacruz@gmail.com>
2023-02-22 01:16:19 +0800

    Implemented the parsing of maze in Maze.java

--- src/core/maze/Maze.java
+++ src/core/maze/Maze.java
@@ -2,6 +2,10 @@ package core.maze;
 
 import lombok.Setter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Maze 
 {
     Cell[][] maze;
@@ -20,9 +24,25 @@ public class Maze
         this.size = size;
     }
 
-    public Maze(String filename)
-    {
-        
+    public Maze(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line = reader.readLine();
+        size = line.length();
+        maze = new Cell[size][size];
+        int row = 0;
+        do {
+            for (int col = 0; col < size; col++) {
+                char c = line.charAt(col);
+                Cell.Type type = c == '#' ? Cell.Type.WALL : Cell.Type.SPACE;
+                maze[row][col] = new Cell(row, col, type);
+                if (c == 'S') {
+                    initialCell = maze[row][col];
+                } else if (c == 'G') {
+                    goalCell = maze[row][col];
+                }
+            }
+            row++;
+        } while ((line = reader.readLine()) != null);
     }
 
     public Cell getCell(int row, int col)
